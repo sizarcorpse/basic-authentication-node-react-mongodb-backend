c9f86f3c9c4883d7e79fdce7fa0e9aa85d82246f
sizarcorpse <74streetmafia@gmail.com>
2020-08-13 01:10:44 +0600

    bawnrm v-1.0.0

---
+++ src/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+const middlewares = require("./middlewares/middlewares");
+
+const app = express();
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    // eslint-disable-next-line comma-dangle
+    credentials: true,
+  })
+);
+
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log("connected to mongoDB")
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const authRoute = require("./api/routes/user/Authentication");
+app.use("/api/user", authRoute);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Basic Authentication w/ Node | React | MongoDB",
+  });
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 3333;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
