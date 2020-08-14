e7df5748f44554c35c0ae4b2314c752cf415316c
sizarcorpse <74streetmafia@gmail.com>
2020-08-14 07:08:30 +0600

    bawnrm v-1.0.1

--- src/server.js
+++ src/server.js
@@ -7,8 +7,24 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const middlewares = require("./middlewares/middlewares");
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
 
 const app = express();
+app.use(allowCrossDomain);
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(cookieParser());
